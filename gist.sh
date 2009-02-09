c75abd29950c2caf36395929a19d53e2cb98485b
Raman Gupta <raman@rocketraman.com>
2009-02-09 16:38:20 -0500

    Read/write data directly from files rather than stdin. Allow setting language.
    Take new parameter -f to read/write to files directly. When using
    -f github will parse the extension to set the gist language. When using
    standard input, -e can be used to set the language.

--- gist.sh
+++ gist.sh
@@ -22,52 +22,90 @@ help ()
 * Posting to GitHub:
   $ cat file|gist.sh
 or
-  $ gist.sh  < file 
+  $ gist.sh < file
+or
+  $ gist.sh -f file
+
+  * When reading from standard input, the gist language can be set with -e:
+    $ gist.sh -e java < file
+
+  * When reading from a file, github guesses the language based on the filename extension
 
 * Getting from GitHub:
   $ gist.sh 1234
+or
+  $ gist.sh -f file 1234
 '
 }
 
 gist_get () 
 {
   URL="https://gist.github.com/$1.txt"
-  GIST=$(curl -s $URL)
+  log "* reading Gist from $URL"
+  CMD="curl -s $URL"
 
-  log "* reading Gist from $URL\n\n" 
-  echo "$GIST"
+  if [ "$_FILENAME" = "" ]; then
+    log "\n"
+    echo "$($CMD)"
+  else
+    if [ -f "$_FILENAME" ]; then
+      log "* Filename $_FILENAME already exists, aborting."
+      exit 1
+    fi
+    $CMD > $_FILENAME
+    log "* Gist written to file $_FILENAME"
+  fi
 }
 
-gist_post () 
+gist_post ()
 {
-  log "* readin Gist from stdin" 
+  if [ "$_FILENAME" = "" ]; then
+    log "* readin Gist from stdin"
+    REQUEST_FILE=/tmp/gist.sh.req
 
-  REQUEST_FILE=/tmp/gist.sh.req
-  RESPONSE_FILE=/tmp/gist.sh.res
+    #cleanup
+    : > $REQUEST_FILE
 
-  #cleanup
-  : > $RESPONSE_FILE
-  : > $REQUEST_FILE
+    # read file content
+    #TODO: improve
+    OLDIFS="$IFS"
+    IFS=""
+    while read line; do
+      echo "$line" >> $REQUEST_FILE
+    done
+    IFS="$OLDIFS"
 
-  # read file content
-  #TODO: improve
-  OLDIFS="$IFS"
-  IFS=""
-  while read line; do
-    echo "$line" >> $REQUEST_FILE
-  done
-  IFS="$OLDIFS"
+    FILENAME=""
+    FILEEXT=".$_FILEEXT"
+  else
+    log "* readin Gist from $_FILENAME"
+
+    REQUEST_FILE="$_FILENAME"
+
+    FILENAME=$(basename "$_FILENAME")
+
+    if [ "$_FILEEXT" != "" ]; then
+      log "* warning: -e specified with -f, ignoring -e"
+    fi
+
+    FILEEXT=""
+  fi
 
   if [ ! -s $REQUEST_FILE ]; then
     help && exit 0
   fi
-    
- curl http://gist.github.com/gists \
+
+  RESPONSE_FILE=/tmp/gist.sh.res
+
+  #cleanup
+  : > $RESPONSE_FILE
+
+  curl http://gist.github.com/gists \
        -i \
        --silent \
        --data-urlencode private=on  \
-       --data-urlencode 'file_name[gistfile1]=' \
-       --data-urlencode 'file_ext[gistfile1]=' \
+       --data-urlencode "file_name[gistfile1]=$FILENAME" \
+       --data-urlencode "file_ext[gistfile1]=$FILEEXT" \
        --data-urlencode "file_contents[gistfile1]@$REQUEST_FILE" \
        -o $RESPONSE_FILE
 
@@ -78,14 +116,25 @@ gist_post ()
 
 require curl
 
-case $1 in
+while [ $# -gt 0 ]; do
+  case $1 in
   -h|--help)
-    help
+      help
+      exit 0
   ;;
-  "")
-    gist_post
+  -e|--ext)
+      shift
+      _FILEEXT="$1"
+  ;;
+  -f|--file)
+      shift
+      _FILENAME="$1"
   ;;
   *[a-zA-Z0-9]) # gist ID
-    gist_get $1
+      gist_get $1
+      exit 0
   ;;
-esac
+  esac
+  shift
+done
+gist_post
